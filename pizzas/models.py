5617d25ac292c6f6b434b239f7285d89dd58e217
Thomas Wiffler <thomas@thomas.lan1>
2020-05-06 03:39:37 -0500

    Added pizzas and pizza view to see toppings

--- pizzas/models.py
+++ pizzas/models.py
@@ -11,6 +11,7 @@ class Pizza(models.Model):
 class Topping(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     name = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.text[:50]}..."
+        return f"{self.name[:50]}..."
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Pizza, Topping
 # Create your views here.
+def index(request):
+    """The home page for Pizzas."""
+    return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    """Show all pizza options available."""
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    """Show a single pizza and all its toppings."""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('-date_added')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas/pizza.html', context)
