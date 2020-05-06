fa6390f9bb7f57b933956c65b61d2bdbfaa2ada7
Thomas Wiffler <thomas@thomas.lan1>
2020-05-06 01:22:47 -0500

    Initial commit Pizza and Topping models created

---
+++ pizzas/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
